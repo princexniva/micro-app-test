9f5e20a26612c2db18ecd7345d835fb22ed61cd6
princexniva <sanjivy3517@gmail.com>
2024-03-26 17:19:33 +0530

    added main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+print("hello, micro!")
+
+print("running python code...")
+a = input("<get-input>Enter a number: \n")
+print("Enter number again: ")
+b = input("<get-input>\n")
+print("The sum is:", a+b)
+print("bye!")
