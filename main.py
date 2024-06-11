5d085735eab8b07a41446964388240bacbafeaed
princexniva <sanjivy3517@gmail.com>
2024-06-12 01:19:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def sum(a, b):
 a = int(input('Enter 1st number: '))
 b = int(input('Enter 2nd number: '))
 
-print("<h1>Hello Sanjivy</h1>")
+print('<h1 style="color: red; font-size: 100px;">Hello</h1>')
+print('<h3 style="color: red;">Hello</h3>')
 
 print(f'Sum of {a} and {b} is {sum(a, b)}')
